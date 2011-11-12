f2e4612a9de8744b2d8433f08e06a2b5ee4f75e4
tranxete <tranxete@da0ffba8-1bf6-0d5b-78db-04a6448e1aed>
2011-11-12 03:58:45 +0000

    Main layout

---
+++ trunk/children/src/finappers/eazybk/child/Dashboard.java
@@ -0,0 +1,14 @@
+package finappers.eazybk.child;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Dashboard extends Activity {
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+
+}
