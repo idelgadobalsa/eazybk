c73f3fa6eecfe835acc1492d49c77528490b0da1
tyler.bcn <tyler.bcn@gmail.com>
2011-11-12 11:12:27 +0000

    Very alpha version, last commit before FinAppsParty exposition

--- children/src/finappers/eazybk/child/Dashboard.java
+++ children/src/finappers/eazybk/child/Dashboard.java
@@ -1,7 +1,13 @@
 package finappers.eazybk.child;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import finappers.eazybk.child.data.StaticChildDAO;
+import finappers.eazybk.common.business.BasicAccount;
+import finappers.eazybk.common.data.StaticDAOFactory;
 
 public class Dashboard extends Activity {
 	/** Called when the activity is first created. */
@@ -9,6 +15,40 @@ public class Dashboard extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+        
+        
+        StaticChildDAO dao = (StaticChildDAO) StaticDAOFactory.getInstance().getChildDAO();
+        
+        BasicAccount account = (BasicAccount) dao.getAccount(0);
+        TextView balance;
+        
+        if (account != null) {
+        	balance = (TextView) findViewById(R.id.cashBalance);
+        	balance.setText(Float.toString(account.getBalance()));
+        }
+        
+        account = (BasicAccount) dao.getAccount(1);
+        
+        if (account != null) {
+        	balance = (TextView) findViewById(R.id.savingsBalance);
+        	balance.setText(Float.toString(account.getBalance()));
+        }
+         
+    }
+    
+    public void addIncome(View view) {
+    	Intent transactionIntent = new Intent(view.getContext(), ModifyCash.class);     	
+    	transactionIntent.putExtra("transactionType", 1);
+    	
+    	startActivity(transactionIntent);
+    	
+    }
+    
+    public void subtractExpense(View view) {
+    	Intent transactionIntent = new Intent(view.getContext(), ModifyCash.class);     	
+    	transactionIntent.putExtra("transactionType", -1);
+    	
+    	startActivity(transactionIntent);
     }
 
 }
--- children/src/finappers/eazybk/child/MainActivity.java
+++ children/src/finappers/eazybk/child/MainActivity.java
@@ -1,7 +1,13 @@
 package finappers.eazybk.child;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import finappers.eazybk.common.auth.AuthResult;
+import finappers.eazybk.common.auth.Authenticator;
+import finappers.eazybk.common.auth.FakeAuthAdapter;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +15,26 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+    }
+    
+    public void submitAuth(View view) {
+    	
+    	EditText userName = (EditText) findViewById(R.id.input_username);
+    	EditText password = (EditText) findViewById(R.id.input_password);
+    	
+    	FakeAuthAdapter fakeAdapter = new FakeAuthAdapter();
+    	fakeAdapter.setCredentials(userName.getEditableText().toString(), password.getEditableText().toString());
+    	AuthResult result = Authenticator.getInstance().authenticate(fakeAdapter);   	
+    	
+    	if (result.isSuscessful()) {
+    		startActivity(new Intent(view.getContext(), Dashboard.class));
+    	} else {
+    		
+    	}
+    	
+    	//startActivity(new Intent(view.getContext(), Dashboard.class));
+    	
+    	
     }
 }
