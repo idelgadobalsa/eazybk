a4cdfda96f379da34ad2917d304d6153863d282e
tyler.bcn@gmail.com <tyler.bcn@gmail.com@da0ffba8-1bf6-0d5b-78db-04a6448e1aed>
2011-11-12 11:08:36 +0000

    Changing the transaction type name

--- trunk/children/src/finappers/eazybk/child/ModifyCash.java
+++ trunk/children/src/finappers/eazybk/child/ModifyCash.java
@@ -2,12 +2,25 @@ package finappers.eazybk.child;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ModifyCash extends Activity {
 	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.modifycash);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.modifycash);
+
+		byte transactionType = getIntent().getExtras().getByte("transactionType");
+				
+		TextView transactionName = (TextView) findViewById(R.id.transactionTypeName);
+
+		if (transactionType > 0) {
+			transactionName.setText("Incomming");
+		} else if (transactionType < 0) {
+			transactionName.setText("Expense");
+		} else {
+			transactionName.setText("Unknow");
+		}
+	}
 }
