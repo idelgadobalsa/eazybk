b6bdf15a81c146c07a6dea3d1f260e7b7aff16ff
tyler.bcn <tyler.bcn@gmail.com>
2011-11-12 07:14:14 +0000

    Basic data classes implementation

---
+++ parents/src/finappers/eazybk/parent/business/ChildInterface.java
@@ -0,0 +1,10 @@
+package finappers.eazybk.parent.business;
+
+import java.util.List;
+
+public interface ChildInterface {
+	String getName();
+	List<?> getAccounts();
+	List<?> getLoansRequest();
+	List<?> getLoans();
+}
